36ae1e62cfae2f60abaff9abe27593eadf998939
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-10 19:39:56 +0530

    add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,38 @@
-import type { Component } from 'solid-js';
+import {QueryClientProvider} from "@tanstack/solid-query";
+import {Component, createEffect, createSignal, Show,} from 'solid-js';
+import UserSignup from "./authentication";
+import OrganizationRegistration from './registration';
+import {supabase} from "./supabase";
+import {QueryClientInstance} from "./tanstack_query";
 
 const App: Component = () => {
-  return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-  );
+    const [getAuthState, setAuthState] = createSignal(false)
+
+    createEffect(async () => {
+        supabase.auth.onAuthStateChange((e, session) => {
+            if (!session) {
+                setAuthState(false)
+            } else {
+                setAuthState(true)
+            }
+        })
+    })
+
+    return (
+        <QueryClientProvider client={QueryClientInstance}>
+            <div class={'w-full h-screen min-h-screen mx-auto bg-gradient-to-b from-v-bg to-v-accent'}>
+                <Show when={getAuthState()}
+                      fallback={
+                          <UserSignup/>
+                      }>
+                    <Show when={true}>
+                        <OrganizationRegistration/>
+                    </Show>
+                </Show>
+            </div>
+        </QueryClientProvider>
+    )
+
 };
 
 export default App;
