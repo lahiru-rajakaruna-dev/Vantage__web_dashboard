e0c27b8124bd012a5d3818dc7bcbc8f25f5fd590
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-15 07:19:06 +0530

    add loading screen

---
+++ src/common_components/LoadingScreen.tsx
@@ -0,0 +1,9 @@
+export default function LoadingScreen() {
+    return (
+        <div class={'absolute inset-0 w-full h-full text-3xl font-bold'}>
+            <div class={"absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl font-bold"}>
+                Loading...
+            </div>
+        </div>
+    )
+}
