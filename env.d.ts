3f8cfe2b03116c2aff9841af13401b9ae94cbb7a
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-15 07:20:39 +0530

    updated

--- env.d.ts
+++ env.d.ts
@@ -4,7 +4,10 @@ interface ImportMetaEnv {
     readonly VITE_API_URL: string,
     readonly VITE_SUPABASE_URL: string,
     readonly VITE_SUPABASE_PUBLISHABLE_KEY: string,
-    readonly VITE_SUPABASE_PROJECT_ID: string
+    readonly VITE_SUPABASE_PROJECT_ID: string,
+    readonly VITE_SUPABASE_STORAGE_BUCKET_ID: string,
+    readonly VITE_SUPABASE_STORAGE_BUCKET_ACCESS_KEY: string,
+    readonly VITE_SUPABASE_STORAGE_BUCKET_URL: string,
 }
 
 interface ImportMeta {
