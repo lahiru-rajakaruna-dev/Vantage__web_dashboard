6c5e1532675cbd9568d47fb44a50767e837aa433
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-19 17:58:27 +0530

    removed unused header

--- src/wretch/index.ts
+++ src/wretch/index.ts
@@ -4,5 +4,5 @@ export const api = wretch(import.meta.env.VITE_API_URL, {
     cache      : 'no-cache',
     credentials: 'include',
     priority   : 'high',
-    mode       : 'cors'
+    mode       : 'cors',
 })
