d5863733521b19b4e3c03eec23c9d239b4455ecc
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-19 18:04:47 +0530

    add registration check

--- src/Screen.tsx
+++ src/Screen.tsx
@@ -1,45 +1,62 @@
 import {User}                                                                        from "@supabase/supabase-js";
-import {createEffect, createSignal, onMount, Show,}                                  from 'solid-js';
+import {createEffect, createResource, createSignal, onMount, Show,}                  from 'solid-js';
 import App                                                                           from "./App";
 import LoadingScreen                                                                 from "./common_components/LoadingScreen";
 import {CNTXAuth}                                                                    from "./contexts/cntx_auth";
 import {attachCallbackToAuthStateChange, checkUserSession, fetchSupabaseUserProfile} from "./supabase/authentication";
+import {api}                                                                         from "./wretch";
 
 export default function Screen() {
     const [getIsAuthenticated, setIsAuthenticated]         = createSignal(false)
     const [getIsLoading, setIsLoading]                     = createSignal(true)
     const [getSupabaseUserProfile, setSupabaseUserProfile] = createSignal<User>()
+    const [getIsRegistered, actions]                       = createResource(
+        getIsAuthenticated, async (isAuthenticated) => {
+            if (!isAuthenticated) {
+                return false
+            }
+
+            const response = await api.get('/organization/is_registered')
+                                      .json<{
+                                          isRegistered: boolean
+                                      }>()
+
+            return response.isRegistered
+        }, {
+            initialValue: false,
+        })
 
     onMount(async () => {
-        const session = await checkUserSession()
+        try {
+            const session = await checkUserSession()
 
-        if (import.meta.env.DEV) {
-            console.debug("Screen: ", session)
-        }
-
-        if (!session) {
-            setIsAuthenticated(false)
-        } else {
-            const supabaseUserProfile = await fetchSupabaseUserProfile()
-            setSupabaseUserProfile(supabaseUserProfile)
-            setIsAuthenticated(true)
-        }
-
-
-        attachCallbackToAuthStateChange((e, session) => {
             if (import.meta.env.DEV) {
-                console.debug("Screen: ", e, session)
+                console.debug("Screen: ", session)
             }
 
             if (!session) {
                 setIsAuthenticated(false)
             } else {
+                const supabaseUserProfile = await fetchSupabaseUserProfile()
+                setSupabaseUserProfile(supabaseUserProfile)
                 setIsAuthenticated(true)
             }
-        })
 
-        setIsLoading(false)
+            attachCallbackToAuthStateChange(async (e, supabaseSession) => {
+                if (import.meta.env.DEV) {
+                    console.debug("Screen: ", e, supabaseSession)
+                }
 
+                if (!supabaseSession) {
+                    setIsAuthenticated(false)
+                    return
+                }
+
+                setIsAuthenticated(true)
+            })
+        } finally {
+            setIsLoading(false)
+        }
         // return data.subscription.unsubscribe()
     })
 
@@ -58,7 +75,7 @@ export default function Screen() {
     return (
         <CNTXAuth.Provider value={{
             getIsAuthed           : getIsAuthenticated,
-            getIsRegistered       : () => false,
+            getIsRegistered       : getIsRegistered,
             getSupabaseUserProfile: getSupabaseUserProfile,
         }}>
             <Show
