c20cd627630745987c4685ca3117771587c3de4e
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-31 21:03:05 +0530

    init tanstack query

--- src/tanstack_query/index.ts
+++ src/tanstack_query/index.ts
@@ -1,3 +1,39 @@
-import {QueryClient} from "@tanstack/solid-query"
+import { QueryClient } from '@tanstack/solid-query'
 
-export const QueryClientInstance = new QueryClient()
+
+
+
+
+let queryClient: QueryClient;
+
+
+export function getQueryClientInstance() {
+    if (!queryClient) {
+        queryClient = new QueryClient({
+                                          defaultOptions: {
+                                              queries  : {
+                                                  refetchOnMount: true,
+                                                  gcTime        : 0,
+                                                  staleTime     : 0,
+                                                  throwOnError  : false,
+                                                  retry         : 3,
+                                                  retryOnMount  : true,
+                                                  retryDelay    : 1000 * 30
+                                                  
+                                              },
+                                              mutations: {
+                                                  onError({ cause, message, name, stack }) {
+                                                      if (import.meta.env.DEV) {
+                                                          console.debug(stack)
+                                                      }
+                                                      console.error({ name, message, cause })
+                                                  }
+                                              }
+                                          }
+                                          
+                                      }
+        )
+    }
+    
+    return queryClient
+}
