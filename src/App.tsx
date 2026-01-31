a421c9925b017ab28756c06d6dd193803920bcce
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-31 21:05:05 +0530

    moved queryClient higher on the tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,31 @@
-import {QueryClientProvider}    from "@tanstack/solid-query";
-import {Show,}                  from "solid-js";
-import {Toaster}                from "solid-toast";
-import UserSignup               from "./authentication";
-import {useCNTXAuth}            from "./contexts/cntx_auth";
-import OrganizationRegistration from "./registration";
-import {QueryClientInstance}    from "./tanstack_query";
+import { Show, }                from 'solid-js';
+import { Toaster }              from 'solid-toast';
+import UserSignup               from './authentication';
+import { useCNTXAuth }          from './contexts/cntx_auth';
+import OrganizationRegistration from './registration';
+import Views                    from './views';
+
+
 
 export default function App() {
     const {
               getIsAuthed,
               getIsRegistered
           } = useCNTXAuth()
-
-    return (
-        <QueryClientProvider client={QueryClientInstance}>
-            <div class={'w-full h-screen min-h-screen mx-auto bg-gradient-to-b from-v-bg to-v-accent'}>
-                <Toaster/>
-                <Show when={getIsAuthed()}
-                      fallback={
-                          <UserSignup/>
-                      }>
-                    <Show
-                        when={getIsRegistered()}
-                        fallback={
-                            <OrganizationRegistration/>
-                        }>
-                        <div>
-                            DASHBOARD
-                        </div>
-                    </Show>
-                </Show>
-            </div>
-        </QueryClientProvider>
-    )
+    
+    return (<div class={ 'w-full h-screen min-h-screen mx-auto bg-gradient-to-b from-v-bg to-v-accent' }>
+        <Toaster/>
+        <Show
+                when={ getIsAuthed() }
+                fallback={ <UserSignup/> }
+        >
+            <Show
+                    when={ getIsRegistered() }
+                    // when={ true } // FOR DEVELOPMENT
+                    fallback={ <OrganizationRegistration/> }
+            >
+                <Views/>
+            </Show>
+        </Show>
+    </div>)
 }
