98a534c3d2cca715de279da318ad8d2c8e9b4dac
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-01-31 21:05:27 +0530

    formatting changes

--- src/Screen.tsx
+++ src/Screen.tsx
@@ -1,39 +1,53 @@
-import {User}                                                                        from "@supabase/supabase-js";
-import {createEffect, createResource, createSignal, onMount, Show,}                  from 'solid-js';
-import App                                                                           from "./App";
-import LoadingScreen                                                                 from "./common_components/LoadingScreen";
-import {CNTXAuth}                                                                    from "./contexts/cntx_auth";
-import {attachCallbackToAuthStateChange, checkUserSession, fetchSupabaseUserProfile} from "./supabase/authentication";
-import {api}                                                                         from "./wretch";
+import { User }                                                                        from '@supabase/supabase-js';
+import { useQuery }                                                                    from '@tanstack/solid-query';
+import { createEffect, createSignal, onMount, Show, }                                  from 'solid-js';
+import App                                                                             from './App';
+import LoadingScreen
+                                                                                       from './common_components/LoadingScreen';
+import { CNTXAuth }                                                                    from './contexts/cntx_auth';
+import { attachCallbackToAuthStateChange, checkUserSession, fetchSupabaseUserProfile } from './supabase/authentication';
+import api                                                                             from './wretch/api';
+
+
 
 export default function Screen() {
-    const [getIsAuthenticated, setIsAuthenticated]         = createSignal(false)
-    const [getIsLoading, setIsLoading]                     = createSignal(true)
-    const [getSupabaseUserProfile, setSupabaseUserProfile] = createSignal<User>()
-    const [getIsRegistered, actions]                       = createResource(
-        getIsAuthenticated, async (isAuthenticated) => {
-            if (!isAuthenticated) {
+    const [ getIsAuthenticated, setIsAuthenticated ]         = createSignal(false)
+    const [ getIsLoading, setIsLoading ]                     = createSignal(true)
+    const [ getSupabaseUserProfile, setSupabaseUserProfile ] = createSignal<User>()
+    const queryIsRegistered                                  = useQuery(() => {
+        return {
+            queryKey: [ 'is_registered', localStorage.getItem('user_id') ],
+            async queryFn() {
+                try {
+                    setIsLoading(true)
+                    const response = await api.organization.isRegistered()
+                    return response.isRegistered
+                } finally {
+                    setIsLoading(false)
+                }
+            },
+            retry      : 3,
+            initialData: () => {
                 return false
-            }
-
-            const response = await api.get('/organization/is_registered')
-                                      .json<{
-                                          isRegistered: boolean
-                                      }>()
-
-            return response.isRegistered
-        }, {
-            initialValue: false,
-        })
-
+            },
+            enabled    : getSupabaseUserProfile() ? true : false
+        }
+    })
+    
+    createEffect(() => {
+        if (getSupabaseUserProfile()) {
+            queryIsRegistered.refetch()
+        }
+    })
+    
     onMount(async () => {
         try {
             const session = await checkUserSession()
-
+            
             if (import.meta.env.DEV) {
-                console.debug("Screen: ", session)
+                console.debug('Screen: ', session)
             }
-
+            
             if (!session) {
                 setIsAuthenticated(false)
             } else {
@@ -41,52 +55,54 @@ export default function Screen() {
                 setSupabaseUserProfile(supabaseUserProfile)
                 setIsAuthenticated(true)
             }
-
+            
             attachCallbackToAuthStateChange(async (e, supabaseSession) => {
                 if (import.meta.env.DEV) {
-                    console.debug("Screen: ", e, supabaseSession)
+                    console.debug('Screen: ', e, supabaseSession)
                 }
-
+                
                 if (!supabaseSession) {
                     setIsAuthenticated(false)
                     return
                 }
-
+                
                 setIsAuthenticated(true)
             })
-        } finally {
-            setIsLoading(false)
+        } catch (e) {
+            console.error('Authentication Failed...')
+            if (import.meta.env.DEV) {
+                console.debug(e)
+            }
         }
         // return data.subscription.unsubscribe()
     })
-
+    
     createEffect(async () => {
         const user = getSupabaseUserProfile()
-
+        
         if (!user) {
             await window.cookieStore.delete('user_id')
             return
         }
-
-        await window.cookieStore.set("user_id", user.id)
+        
+        await window.cookieStore.set('user_id', user.id)
     })
-
-
-    return (
-        <CNTXAuth.Provider value={{
-            getIsAuthed           : getIsAuthenticated,
-            getIsRegistered       : getIsRegistered,
-            getSupabaseUserProfile: getSupabaseUserProfile,
-        }}>
-            <Show
-                when={!getIsLoading()}
-                fallback={
-                    <LoadingScreen/>
-                }>
-                <App/>
-            </Show>
-        </CNTXAuth.Provider>
-    )
-
+    
+    return (<CNTXAuth.Provider
+            value={ {
+                getIsAuthed           : getIsAuthenticated,
+                getIsRegistered       : () => queryIsRegistered.data,
+                getSupabaseUserProfile: getSupabaseUserProfile,
+            } }
+    >
+        <Show
+                when={ !getIsLoading() }
+                // when={true} // FOR DEVELOPMENT
+                fallback={ <LoadingScreen/> }
+        >
+            <App/>
+        </Show>
+    </CNTXAuth.Provider>)
+    
 }
 
