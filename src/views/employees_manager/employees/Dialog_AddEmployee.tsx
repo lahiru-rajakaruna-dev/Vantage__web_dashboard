3fd969cc129a312967be51a599b06d8718f061a9
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-02 22:14:19 +0530

    create: add employee, update employee dialogs

---
+++ src/views/employees_manager/employees/Dialog_AddEmployee.tsx
@@ -0,0 +1,202 @@
+import { Dialog }                      from '@kobalte/core/dialog';
+import { useMutation, useQueryClient } from '@tanstack/solid-query';
+import { FaSolidUserPlus }             from 'solid-icons/fa';
+import { IoClose }                     from 'solid-icons/io';
+import { createSignal }                from 'solid-js';
+import { toast }                       from 'solid-toast';
+import PrimaryButton                   from '../../../common_components/PrimaryButton';
+import TextInput                       from '../../../common_components/TextInput';
+import api                             from '../../../wretch/api';
+import { Employee }                    from '../../../wretch/api/types';
+
+
+
+export default function Dialog_AddEmployee(props: {
+    getIsBusy: () => boolean,
+    setIsBusy: (state: boolean) => void,
+    getIsOpen: () => boolean,
+    setIsOpen: (state: boolean) => void
+}) {
+    let timeOutId: NodeJS.Timeout;
+    const [ getNewEmployeeUsername, setNewEmployeeUsername ] = createSignal('')
+    const [ getNewEmployeePassword, setNewEmployeePassword ] = createSignal('')
+    
+    const queryClient            = useQueryClient()
+    const addNewEmployeeMutation = useMutation(() => {
+        return {
+            mutationKey: [ 'employee', 'add' ],
+            async mutationFn() {
+                return await api.employeeApi.create({
+                                                        employees_credentials_username: getNewEmployeeUsername()
+                                                                .toString(),
+                                                        employees_credentials_password: getNewEmployeePassword()
+                                                                .toString()
+                                                    })
+            },
+            onMutate : async () => {
+                props.setIsBusy(true)
+                queryClient.setQueryData<unknown, string[], Employee[]>(
+                        [ 'employees' ],
+                        (currentEmployees) => {
+                            if (currentEmployees &&
+                                currentEmployees.length) {
+                                return [
+                                    ...(currentEmployees as Array<any>),
+                                    {
+                                        employee_id      : 'temp-employee-1',
+                                        employee_username: getNewEmployeeUsername(),
+                                        employee_phone   : 'no-set',
+                                    }
+                                ]
+                            }
+                            
+                            return [
+                                {
+                                    employee_id      : 'temp-employee-1',
+                                    employee_username: getNewEmployeeUsername(),
+                                    employee_phone   : 'no-set',
+                                }
+                            ]
+                        }
+                )
+                
+            },
+            onSettled: async () => {
+                props.setIsBusy(false)
+                
+            },
+            retry    : false,
+            onSuccess: async (data) => {
+                await queryClient.setQueryData([ 'employees' ], data)
+                toast.success('New Employee Added',)
+            }
+            
+        }
+    })
+    
+    return (<Dialog
+            open={ props.getIsOpen() }
+            onOpenChange={ props.setIsOpen }
+    >
+        <Dialog.Portal>
+            <Dialog.Overlay class={ 'bg-teal-400/30' }/>
+            <div class={ 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' }>
+                <Dialog.Content class={ 'flex flex-col items-stretch justify-start bg-teal-200 border-2 border-teal-400 rounded-md' }>
+                    <div class={ 'flex flex-row items-center justify-between gap-12 border-b-2 border-teal-400' }>
+                        <Dialog.Title class={ 'p-4 font-semibold' }>
+                            Add New Employee
+                        </Dialog.Title>
+                        <Dialog.CloseButton class={ 'p-4 border-l-2 border-teal-400' }>
+                            <IoClose size={ 16 }/>
+                        </Dialog.CloseButton>
+                    </div>
+                    <div class={ 'p-4 flex flex-col items-stretch justify-start gap-2' }>
+                        {/*<Select*/ }
+                        {/*        class={ 'flex flex-col items-stretch justify-start gap-1' }*/ }
+                        {/*        options={ [*/ }
+                        {/*            'Mannar',*/ }
+                        {/*            'Hambantota',*/ }
+                        {/*            'Kurunegala',*/ }
+                        {/*            'Kalmunai',*/ }
+                        {/*            'Kuliyapitiya',*/ }
+                        {/*            'Moratuwa',*/ }
+                        {/*            'Kalmunai',*/ }
+                        {/*            'Maharagama',*/ }
+                        {/*            'Sri Jayawardenepura Kotte',*/ }
+                        {/*            'Dehiwala-mount Lavinia'*/ }
+                        {/*        ] }*/ }
+                        {/*        placeholder={ <pre>Select A Territory</pre> }*/ }
+                        {/*        itemComponent={ (props) => {*/ }
+                        {/*            return <Select.Item*/ }
+                        {/*                    item={ props.item }*/ }
+                        {/*                    class={ 'px-3 py-2 flex flex-row items-center justify-start gap-4 hover:bg-teal-100 rounded-md cursor-pointer' }*/ }
+                        {/*            >*/ }
+                        {/*                <Select.ItemLabel>*/ }
+                        {/*                    <pre>{ props.item.textValue }</pre>*/ }
+                        {/*                </Select.ItemLabel>*/ }
+                        {/*                <Select.ItemIndicator>*/ }
+                        {/*                    <BsDot size={ 14 }/>*/ }
+                        {/*                </Select.ItemIndicator>*/ }
+                        {/*            </Select.Item>*/ }
+                        {/*        } }*/ }
+                        {/*        onChange={ (value) => {*/ }
+                        {/*            if (!value) {*/ }
+                        {/*                return*/ }
+                        {/*            }*/ }
+                        {/*            */ }
+                        {/*            setNewEmployeePassword(value)*/ }
+                        {/*        } }*/ }
+                        {/*>*/ }
+                        {/*    <Select.Label>*/ }
+                        {/*        Assign a Territory*/ }
+                        {/*    </Select.Label>*/ }
+                        {/*    <Select.Trigger class={ 'px-3 py-2 w-full h-fit flex flex-row items-center justify-between gap-4 bg-teal-200 border-2 border-teal-400 rounded-md' }>*/ }
+                        {/*        <Select.Value>*/ }
+                        {/*            { (state) => {*/ }
+                        {/*                const value = state.selectedOption()*/ }
+                        {/*                if (value) {*/ }
+                        {/*                    return <>{ value }</>*/ }
+                        {/*                }*/ }
+                        {/*                return <>No Value</>*/ }
+                        {/*            }*/ }
+                        {/*            */ }
+                        {/*            }*/ }
+                        {/*        </Select.Value>*/ }
+                        {/*        <Select.Icon>*/ }
+                        {/*            <FaSolidSort size={ 14 }/>*/ }
+                        {/*        </Select.Icon>*/ }
+                        {/*    </Select.Trigger>*/ }
+                        {/*    <Select.Portal>*/ }
+                        {/*        <Select.Content class={ 'p-2 bg-teal-200 border-2 border-teal-400 rounded-md' }>*/ }
+                        {/*            <Select.Listbox*/ }
+                        {/*                    class={ 'max-h-36 overflow-y-scroll' }*/ }
+                        {/*            />*/ }
+                        {/*        </Select.Content>*/ }
+                        {/*    </Select.Portal>*/ }
+                        {/*</Select>*/ }
+                        <TextInput
+                                onChange={ (value) => {
+                                    if (timeOutId) {
+                                        clearTimeout(timeOutId)
+                                    }
+                                    timeOutId = setTimeout(() => {
+                                        setNewEmployeeUsername(value)
+                                    }, 500)
+                                } }
+                                placeholder={ 'Enter a nic number' }
+                                inputConfig={ { type: 'text' } }
+                        />
+                        <TextInput
+                                onChange={ (value) => {
+                                    if (timeOutId) {
+                                        clearTimeout(timeOutId)
+                                    }
+                                    timeOutId = setTimeout(() => {
+                                        setNewEmployeePassword(value)
+                                    }, 500)
+                                } }
+                                placeholder={ 'Enter a password' }
+                                inputConfig={ { type: 'password' } }
+                        />
+                    </div>
+                    <div class={ 'p-4 flex flex-col items-stretch justify-start' }>
+                        <PrimaryButton
+                                onClick={ () => addNewEmployeeMutation.mutate() }
+                                getIsBusy={ props.getIsBusy }
+                                getIsDisabled={ props.getIsBusy }
+                        >
+                            <PrimaryButton.Icon>
+                                <FaSolidUserPlus size={ 14 }/>
+                            </PrimaryButton.Icon>
+                            <PrimaryButton.Label>
+                                <pre>Add Employee</pre>
+                            </PrimaryButton.Label>
+                        </PrimaryButton>
+                    </div>
+                </Dialog.Content>
+            </div>
+        </Dialog.Portal>
+    </Dialog>)
+}
+
+
---
+++ src/views/employees_manager/employees/Dialog_UpdateEmployee.tsx
@@ -0,0 +1,214 @@
+import { Dialog }                                        from '@kobalte/core/dialog';
+import { Select }                                        from '@kobalte/core/select';
+import { useMutation, useQuery, useQueryClient }         from '@tanstack/solid-query';
+import { BsDot }                                         from 'solid-icons/bs';
+import { FaRegularCircleDot, FaSolidSort, FaSolidTrash } from 'solid-icons/fa';
+import { IoClose }                                       from 'solid-icons/io';
+import { createSignal }                                  from 'solid-js';
+import { toast }                                         from 'solid-toast';
+import PrimaryButton                                     from '../../../common_components/PrimaryButton';
+import TextInput                                         from '../../../common_components/TextInput';
+import api                                               from '../../../wretch/api';
+import { Employee }                                      from '../../../wretch/api/types';
+
+
+
+export default function Dialog_UpdateEmployee(props: {
+    getIsBusy: () => boolean;
+    setIsBusy: (state: boolean) => void
+    getIsOpen: () => boolean;
+    setIsOpen: (state: boolean) => void
+}) {
+    
+    const queryClient                                  = useQueryClient()
+    const [ getSelectedEmployee, setSelectedEmployee ] = createSignal<Employee>()
+    const [ getEmployeeUpdates, setEmployeeUpdates ]   = createSignal<Partial<Employee>>({})
+    const queryEmployees                               = useQuery(() => {
+        return {
+            queryKey: [ 'employees' ],
+            async queryFn() {
+                return await api.employeeApi.getByOrganization()
+            },
+        }
+    })
+    
+    const mutationUpdateEmployee = useMutation(() => {
+        return {
+            mutationKey: [ 'employees', 'update', getSelectedEmployee() ],
+            async mutationFn() {
+                if (!getSelectedEmployee()) {
+                    toast.error('Please Select an Employee to Remove')
+                    return
+                }
+                
+                return await api.employeeApi.update(getEmployeeUpdates())
+            },
+            onMutate : async () => {
+                props.setIsBusy(true)
+                queryClient.setQueryData<string[], string[], Employee[]>([ 'employees' ], (employees) => {
+                    if (!employees || !getSelectedEmployee()) {
+                        return
+                    }
+                    
+                    return employees.map((employee) => {
+                        if (employee.employee_id !== getSelectedEmployee()!.employee_id) {
+                            return employee
+                        }
+                        return {} as Employee // FOR DEBUGGING
+                    })
+                })
+            },
+            onSettled: async () => {
+                props.setIsBusy(false)
+            },
+            retry    : false,
+            onSuccess: async (data) => {
+                await queryClient.setQueryData([ 'employees' ], data)
+                toast.success('Employee updated')
+            },
+            onError  : (e) => {
+                console.debug(e)
+                toast.error('Could not update employee')
+                
+            }
+            
+        }
+    })
+    
+    return (<Dialog
+            open={ props.getIsOpen() }
+            onOpenChange={ props.setIsOpen }
+    >
+        <Dialog.Portal>
+            <Dialog.Overlay class={ 'absolute inset-0 w-full h-full bg-teal-400/10 backdrop-blur-xs' }/>
+            <div class={ 'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-6/12 max-w-xl' }>
+                <Dialog.Content class={ 'w-full flex flex-col items-stretch justify-start bg-teal-200 border-2 border-teal-400 rounded-md' }>
+                    <div class={ 'flex flex-row items-center justify-between gap-12 border-b-2 border-teal-400' }>
+                        <Dialog.Title class={ 'p-4 font-semibold' }>
+                            Update Employee
+                        </Dialog.Title>
+                        <Dialog.CloseButton class={ 'p-4 border-l-2 border-teal-400' }>
+                            <IoClose size={ 16 }/>
+                        </Dialog.CloseButton>
+                    </div>
+                    <div class={ 'p-4 flex flex-col items-stretch justify-start gap-2' }>
+                        <Select
+                                class={ 'flex flex-col items-stretch justify-start gap-1' }
+                                options={ queryEmployees.data?.map((employee) => `${ employee.employee_id.slice(
+                                                  0,
+                                                  6
+                                          ) }-${ employee.employee_first_name }`) ??
+                                          [] }
+                                /*
+                                 value={ `${ getSelectedEmployee()?.employee_first_name } ${ getSelectedEmployee()?.employee_last_name }` }
+                                 */
+                                placeholder={ 'Select an employee to update' }
+                                itemComponent={ ({ item }) => {
+                                    return <Select.Item
+                                            item={ item }
+                                            class={ 'px-3 py-2 w-full flex flex-row items-center justify-start gap-4 hover:bg-teal-100 rounded-md cursor-pointer' }
+                                    >
+                                        <Select.ItemLabel class={ 'w-full' }>
+                                            <pre>{ item.rawValue }</pre>
+                                        </Select.ItemLabel>
+                                        <Select.ItemIndicator class={ 'w-8' }>
+                                            <FaRegularCircleDot size={ 14 }/>
+                                        </Select.ItemIndicator>
+                                    </Select.Item>
+                                } }
+                                onChange={ (employee_id__first_name) => {
+                                    console.debug(employee_id__first_name)
+                                    if (!employee_id__first_name) {
+                                        return
+                                    }
+                                    
+                                    if (!queryEmployees.data) {
+                                        return
+                                    }
+                                    
+                                    const selectedEmployee = queryEmployees.data.find((employee) => {
+                                        return employee.employee_id.slice(0, 6) ===
+                                               employee_id__first_name.split('-')[0]
+                                    })
+                                    
+                                    if (!selectedEmployee) {
+                                        return
+                                    }
+                                    
+                                    setSelectedEmployee(selectedEmployee)
+                                } }
+                        >
+                            <Select.Label>
+                                Select an Employee to Update
+                            </Select.Label>
+                            
+                            <Select.Trigger class={ 'px-3 py-2 w-full h-fit flex flex-row items-center justify-between gap-4 bg-teal-200 border-2 border-teal-400 rounded-md' }>
+                                <Select.Value>
+                                    { (state) => {
+                                        const value = state.selectedOption()
+                                        
+                                        if (value) {
+                                            return <>{ value }</>
+                                        }
+                                        
+                                        return 'No Value'
+                                    }
+                                    
+                                    }
+                                </Select.Value>
+                                <Select.Icon>
+                                    <FaSolidSort size={ 14 }/>
+                                </Select.Icon>
+                            </Select.Trigger>
+                            <Select.Portal>
+                                <Select.Content class={ 'p-2 bg-teal-200 border-2 border-teal-400 rounded-md' }>
+                                    <Select.Listbox/>
+                                </Select.Content>
+                            </Select.Portal>
+                        </Select>
+                        
+                        
+                        { getSelectedEmployee() ?
+                          <div class={ 'p-2 flex flex-row flex-wrap gap-2 gap-y-4 border-2 border-teal-400/60 rounded-md' }>
+                              {
+                                  Object.entries(getSelectedEmployee() ?? {}).map((entry) => {
+                                      const [ key, value ] = entry;
+                                      
+                                      return <div class={ 'flex-4/12' }>
+                                          <TextInput
+                                                  onChange={ (value) => setEmployeeUpdates((prevUpdates) => {
+                                                      return {
+                                                          ...prevUpdates,
+                                                          [key]: value
+                                                      }
+                                                  }) }
+                                                  inputConfig={ { type: 'text' } }
+                                                  value={ value }
+                                                  label={ key }
+                                          />
+                                      </div>
+                                      
+                                  })
+                              }
+                          </div> : <></> }
+                    </div>
+                    
+                    <div class={ 'p-4 flex flex-col items-stretch' }>
+                        <PrimaryButton
+                                onClick={ () => mutationUpdateEmployee.mutate() }
+                                getIsBusy={ props.getIsBusy }
+                                getIsDisabled={ props.getIsBusy }
+                        >
+                            <PrimaryButton.Icon>
+                                <FaSolidTrash size={ 14 }/>
+                            </PrimaryButton.Icon>
+                            <PrimaryButton.Label>
+                                <pre>Update Employee</pre>
+                            </PrimaryButton.Label>
+                        </PrimaryButton>
+                    </div>
+                </Dialog.Content>
+            </div>
+        </Dialog.Portal>
+    </Dialog>)
+}
