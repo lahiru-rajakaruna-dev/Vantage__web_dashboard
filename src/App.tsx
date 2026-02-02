e452ce7e7afec0f79e10abe32dc0f4b8d823e289
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-02 22:05:50 +0530

    remove auth check

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Show, }                from 'solid-js';
 import { Toaster }              from 'solid-toast';
-import UserSignup               from './authentication';
 import { useCNTXAuth }          from './contexts/cntx_auth';
 import OrganizationRegistration from './registration';
 import Views                    from './views';
@@ -16,16 +15,11 @@ export default function App() {
     return (<div class={ 'w-full h-screen min-h-screen mx-auto bg-gradient-to-b from-v-bg to-v-accent' }>
         <Toaster/>
         <Show
-                when={ getIsAuthed() }
-                fallback={ <UserSignup/> }
+                when={ getIsRegistered() }
+                // when={ true } // FOR DEVELOPMENT
+                fallback={ <OrganizationRegistration/> }
         >
-            <Show
-                    when={ getIsRegistered() }
-                    // when={ true } // FOR DEVELOPMENT
-                    fallback={ <OrganizationRegistration/> }
-            >
-                <Views/>
-            </Show>
+            <Views/>
         </Show>
     </div>)
 }
