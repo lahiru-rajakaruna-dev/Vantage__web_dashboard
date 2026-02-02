d939de705f12a91f5808b8cc7178a961d97b1fee
BL4Z3 <lahiru.rajakaruna.dev@gmail.com>
2026-02-02 22:14:53 +0530

    create employee tab

---
+++ src/views/employees_manager/employees/index.tsx
@@ -0,0 +1,36 @@
+import { createSignal }      from 'solid-js';
+import Dialog_AddEmployee    from './Dialog_AddEmployee';
+import Dialog_UpdateEmployee from './Dialog_UpdateEmployee';
+import EmployeesActionsMenu  from './EmployeesActionsMenu';
+import Table                 from './Table';
+
+
+
+export default function Employees() {
+    const [ getIsAddEmployeeDialogVisible, setIsAddEmployeeDialogVisible ]       = createSignal(false)
+    const [ getIsUpdateEmployeeDialogVisible, setIsUpdateEmployeeDialogVisible ] = createSignal(false)
+    const [ getIsBusy, setIsBusy ]                                               = createSignal(false)
+    
+    return (<div class={ 'relative w-full h-full flex flex-col items-stretch justify-start gap-8 overflow-x-visible' }>
+        <Table/>
+        <EmployeesActionsMenu
+                showAddEmployeeDialog={ () => setIsAddEmployeeDialogVisible(true) }
+                showUpdateEmployeeDialog={ () => setIsUpdateEmployeeDialogVisible(true) }
+        />
+        
+        <Dialog_AddEmployee
+                getIsBusy={ getIsBusy }
+                setIsBusy={ setIsBusy }
+                getIsOpen={ getIsAddEmployeeDialogVisible }
+                setIsOpen={ setIsAddEmployeeDialogVisible }
+        />
+        <Dialog_UpdateEmployee
+                getIsBusy={ getIsBusy }
+                setIsBusy={ setIsBusy }
+                getIsOpen={ getIsUpdateEmployeeDialogVisible }
+                setIsOpen={ setIsUpdateEmployeeDialogVisible }
+        />
+    </div>)
+}
+
+
